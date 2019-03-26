07f5edc857e62c7b8e1db7b476345b55c5c00c18
Ryan Froggatt <rfroggatt@ANSGRP-002784.local>
2019-03-26 14:04:42 +0000

    Adding Untested Power Management Script

--- aws_ec2/ec2_power_management.ps1
+++ aws_ec2/ec2_power_management.ps1
@@ -29,3 +29,27 @@ Set-AWSCredentials -AccessKey $AccessKey -SecretKey $SecretKey ;
 Write-Host "[$(get-date -Format "dd/mm/yy hh:mm:ss")] Successfully logged in to AWS Account"
 Write-Host ""
 
+# Get Current Hour
+$Hour = (Get-Date).Hour
+
+#Stop EC2 Instances
+foreach($region in Get-AWSRegion) {
+  foreach($instance in Get-EC2Instance -Region $region.Region -Filter @{ "Name" = "tag" ; "Value" = "AutoOff=$Hour"}) {
+    Write-Host ("[$(get-date -Format "dd/mm/yy hh:mm:ss")] Stopping Instance - "+$instance.InstanceId)
+    Stop-EC2Instance -InstanceId $instance.InstanceId -Region $region.Region
+    Write-Host ("[$(get-date -Format "dd/mm/yy hh:mm:ss")] "+$instance.InstanceId+" Stopped Succesfully")
+  }
+}
+Write-Host "[$(get-date -Format "dd/mm/yy hh:mm:ss")] Instances Stopped"
+
+
+#Start EC2 Instances
+foreach($region in Get-AWSRegion) {
+  foreach($instance in Get-EC2Instance -Region $region.Region -Filter @{ "Name" = "tag" ; "Value" = "AutoOn=$Hour"}) {
+    Write-Host ("[$(get-date -Format "dd/mm/yy hh:mm:ss")] Starting Instance - "+$instance.InstanceId)
+    Start-EC2Instance -InstanceId $instance.InstanceId -Region $region.Region
+    Write-Host ("[$(get-date -Format "dd/mm/yy hh:mm:ss")] "+$instance.InstanceId+" Started Succesfully")
+  }
+}
+Write-Host "[$(get-date -Format "dd/mm/yy hh:mm:ss")] Instances Started"
+Write-Host "[$(get-date -Format "dd/mm/yy hh:mm:ss")] Script Complete"
